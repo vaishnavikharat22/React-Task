cfcfd943debacf15b9d3cdf5269a59b8b19ccd43
Vaishnavi <vaishnavikharat41@gmail.com>
2026-02-21 12:17:11 +0530

    update in rollup-states

--- Rollup-state/src/App.jsx
+++ Rollup-state/src/App.jsx
@@ -1,34 +1,46 @@
-import React from 'react'
+import React, { createContext, useContext } from 'react'
 import { useState } from 'react'
+
+const Bulbcontext = createContext();
+
 const App = () => {
+  const [bulbon, setbulbon] = useState(true)
 
   return (
     <div>
-      <Lightbulb/>
+      <Bulbcontext.Provider value={{
+        bulbon: bulbon,
+        setbulbon:setbulbon
+      }}>
+        <Lightbulb/>
+      </Bulbcontext.Provider>
+      
     </div>
   )
 }
 
 const Lightbulb = () => {
-  const [bulbon, setbulbon] = useState(true)
   return(
     <div>
-      <Bulbon bulbon= {bulbon}></Bulbon>
-      <Bulbonoff setbulbon = {setbulbon}></Bulbonoff>
+      <Bulbon></Bulbon>
+      <Bulbonoff></Bulbonoff>
     </div>
   )
 }
 
-const Bulbon = ({bulbon}) => {
+const Bulbon = () => {
+  const { bulbon } = useContext(Bulbcontext);
   return(
-    <div>
-      {bulbon ? "Bulb on" : "Bulb off"}
+    <div >
+      <img style={{height:200,width:200}} src={bulbon ? "https://www.shutterstock.com/shutterstock/photos/792745633/display_1500/stock-vector-bright-glowing-incandescent-light-bulb-on-a-white-background-792745633.jpg" 
+      : "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRTuzqMrax_TxNQORfOCyju9tQuB1D9tj10pQ&s"} ></img>
     </div>
   )
 }
 
-const Bulbonoff = ({setbulbon}) => {
+const Bulbonoff = () => {
 
+  const { setbulbon } = useContext(Bulbcontext);
   const onoff = () => {
     setbulbon(c => !c)
   }
